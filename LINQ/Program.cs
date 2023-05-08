67c9f00bf20a2df1ae27f2c9796756163c281cf6
Rakin Brown <rakinbrown38@gmail.com>
2023-05-08 15:15:38 -0500

    videogame lists in order of game length

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myVideogames = new List<string>() { "Nanobreaker", "RE4", "Devil May Cry 5", "Metal Gear Solid", "Final Fantasy 7" };
+
+            var orderVideogames = myVideogames.OrderBy(name => name.Length);
+
+            foreach (var game in orderVideogames)
+            {
+                Console.WriteLine(game);
+            }
         }
+
     }
 }
